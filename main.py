259b705e602178972b9182546d9f251e95819410
dawit101 <130009377+dawit101@users.noreply.github.com>
2023-04-06 23:55:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+#This is a test
+#A String is a piece of text, can be written with quatation marks,
+
+print(type("hello my neighbor 23")) #DataType is a string
+
+username = 'supercoder' #the 'supercoder' is a string
+password = 'supersecret'  #the 'supersecret' is a string
+
+long_string = '''
+wow
+0 0
+---
+'''
+
+print(long_string) #You can draw with strings, line 9-13
+
+first_name = "Dawit"
+last_name = "Arefaine"
+
+full_name = first_name + ' ' + last_name 
+
+print(full_name) #to get space between first/last name you need to either add a comment between them like in line 20, or add space between quotation mark and last name, for example " Areafaine"
+
+#String Concatenation
+#This means to add strings together
+
+print('hello' + ' snoop')
